2a560612b05a7bb2c2042d5d0ff5020146fb5be9
Jabran Rafique <hello@jabran.me>
2015-03-20 19:29:41 +0000

    Set conditional Google Analytics

--- wordpress/wp-content/themes/pkgeography/header.php
+++ wordpress/wp-content/themes/pkgeography/header.php
@@ -40,18 +40,24 @@
 	    <meta name="theme-color" content="#009900">
 		<?php wp_head(); ?>
 
-		<!-- Google analytics tracking -->
-		<script>
-		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		  ga('create', 'UA-9809111-8', 'auto');
-		  ga('send', 'pageview');
-		</script>
+		<?php
+		/**
+		 * Google Analytics - Dist environment only
+		 */
+		if ( !ENV_DEV ) :
+		?>
+			<!-- Google analytics tracking -->
+			<script>
+			  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+			  ga('create', 'UA-9809111-8', 'auto');
+			  ga('send', 'pageview');
+			</script>
+		<?php endif; ?>
 	</head>
 	<body>
-
 
 		<!-- header -->
 		<div class="gpk-header">
